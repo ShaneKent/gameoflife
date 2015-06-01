5085a6000773b87c5467d7338b3932e1edc8fa70
ShaneKent <sdkent@calpoly.edu>
2015-05-31 22:19:05 -0700

    Initial commit. Basic Processing board. Basic .gitignore file.

---
+++ Main.java
@@ -0,0 +1,18 @@
+import processing.core.*;
+
+public class Main extends PApplet{
+   
+   public void setup(){
+      size(640, 480);
+      background(color(255, 255, 255));
+   }
+
+   public void draw(){
+      background(color(255, 255, 255));
+   }
+
+   public static void main(String[] args){
+      PApplet.main("Main");
+   }
+
+}
